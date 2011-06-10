25449bdc923b284a4efce6394eb7d548f23d81e8
Andre Merzky <andre@merzky.net>
2011-06-10 14:34:58 +0000

    fix

--- doc/concepts/adaptor/perf.sh
+++ doc/concepts/adaptor/perf.sh
@@ -1,12 +1,15 @@
 #!/bin/sh
 
-for i in 1 2 3 4 5 6 7 8 9 10 ; 
+export LD_LIBRARY_PATH=`pwd`/engine
+
+for i in 0 1 2 3 4 5 6 7 8 9 ; 
   do 
-  for i in 1 2 3 4 5 6 7 8 9 10 ; 
+  for j in 0 1 2 3 4 5 6 7 8 9 ; 
    do 
-    for j in 1 2 3 4 5 6 7 8 9 10 ; 
+    for k in 0 1 2 3 4 5 6 7 8 9 ; 
       do 
-      ./adaptor_test; 
+      echo $i$j$k;
+      ./adaptor_test >/dev/null; 
     done
   done
 done
